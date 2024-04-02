8cfb7c3a39e34a6a085c0eab6468a291bd881612
PGK16POWERS <136324051+PGK16POWERS@users.noreply.github.com>
2024-04-02 20:56:05 +0200

    javaFile

--- location/src/main/java/com/location/location/Routes.java
+++ location/src/main/java/com/location/location/Routes.java
@@ -1,7 +1,23 @@
 package com.location.location;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.core.io.Resource;
+import java.nio.charset.StandardCharsets;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.util.StreamUtils;
+import java.io.IOException;
 
-
+@RestController
 public class Routes {
+
+    @GetMapping("/")
+    public String HomePath() throws IOException {
+       Resource resource = new ClassPathResource("static/index.html");
+
+       String htmlContent = StreamUtils.copyToString(resource.getInputStream(),StandardCharsets.UTF_8);
+
+       return htmlContent;
+    }
     
 }
