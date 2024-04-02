67d4c131f24d4667c6716ed063bf11a1d8bddfa7
PGK16POWERS <136324051+PGK16POWERS@users.noreply.github.com>
2024-04-03 00:00:43 +0200

    js

--- location/src/main/resources/static/javaScriptFiles/location.js
+++ location/src/main/resources/static/javaScriptFiles/location.js
@@ -3,19 +3,23 @@ document.addEventListener("DOMContentLoaded", () => {
     const outputDiv = document.querySelector("#userLocation");
 
     locationButton.addEventListener("click", () => {
-        if(navigator) {
+        if(navigator.geolocation) {
             navigator.geolocation.getCurrentPosition((position) => {
 
                 const longitude = position.coords.longitude;
                 const latitude = position.coords.latitude;
                 const accessToken = "pk.eyJ1IjoicXVpY2sta3V0IiwiYSI6ImNsdWl1Zmp0YzA4cXIyaW85cWx1N2NqYWkifQ.oktkdVTSggmud8PqxSB5dg"
 
-                fetch(`https://api.mapbox.com/search/searchbox/v1/reverse?/${longitude},${latitude}.json?access_token=${accessToken}`)
+                fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${longitude},${latitude}.json?access_token=${accessToken}`)
 
                     .then(response => response.json)
                     .then(data => {
-                        outputDiv.value = data;
-                        console.log(data);
+                        if (data.features && data.features.length > 0) {
+                            outputDiv.value = data.features[0].place_name;
+                            console.log(data.features[0].place_name);
+                        } else {
+                            console.log("Location not found.");
+                        }
                     })
                     .catch((error) => {
                         console.error("Error occurred while fetching your data: ", error);
