8464bd9097dffbf30464d3d2e0812d58ce112fcc
PGK16POWERS <136324051+PGK16POWERS@users.noreply.github.com>
2024-04-03 06:28:05 +0200

    JavaFile

--- location/src/main/java/com/location/location/DatabaseOper.java
+++ location/src/main/java/com/location/location/DatabaseOper.java
@@ -13,5 +13,29 @@ import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
 
 public class DatabaseOper {
+    public  static void main(String[] args) {
+        String connectionString = "mongodb+srv://Marjella:Marjella@diamondburg.lxfxn0d.mongodb.net/?retryWrites=true&w=majority&appName=DiamondBurg";
+
+        ServerApi serverApi = ServerApi.builder()
+                .version(ServerApiVersion.V1)
+                .build();
+
+        MongoClientSettings settings = MongoClientSettings.builder()
+                .applyConnectionString(new ConnectionString(connectionString))
+                .serverApi(serverApi)
+                .build();
+
+        // Create a new client and connect to the server
+        try (MongoClient mongoClient = MongoClients.create(settings)) {
+            try {
+                // Send a ping to confirm a successful connection
+                MongoDatabase database = mongoClient.getDatabase("QuickKut");
+                database.runCommand(new Document("ping", 1));
+                System.out.println("Pinged your deployment. You successfully connected to MongoDB!");
+            } catch (MongoException e) {
+                e.printStackTrace();
+            }
+        }
+    }
     
 }
