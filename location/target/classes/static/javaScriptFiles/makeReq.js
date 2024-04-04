da504ecd353d250730259fc600a997ace6f16b0a
PGK16POWERS <136324051+PGK16POWERS@users.noreply.github.com>
2024-04-04 14:52:50 +0200

    location

--- location/target/classes/static/javaScriptFiles/makeReq.js
+++ location/target/classes/static/javaScriptFiles/makeReq.js
@@ -5,20 +5,23 @@ document.addEventListener("DOMContentLoaded", () => {
 
     reqBtn.addEventListener("click", () => {
         // Make AJAX request to server-side endpoint
-        fetch('/getActiveBarbers') // Assuming your server exposes this endpoint
+        fetch('/active/getActiveBarbers') // Assuming your server exposes this endpoint
             .then(response => response.json())
             .then(data => {
                 // Handle the data received from the server
+                console.log("Data retrieved successfully");
+                console.log(data);
                 // For example, you can iterate over the data and create HTML elements to display it
                 data.forEach(barber => {
                     const barberDiv = document.createElement('div');
                     barberDiv.textContent = `${barber.name}, ${barber.location}`; // Example data fields
                     childDiv.appendChild(barberDiv);
                 });
+                childDiv.style.border = "2px solid green"
             })
             .catch(error => {
                 console.error('Error fetching data:', error);
-                childDiv.textContent = "Error while searching for Barbers"
+                childDiv.textContent = "Error while searching for Barbers: " + error.message;
                 childDiv.style.border = "2px solid orangered"
             });
     })
