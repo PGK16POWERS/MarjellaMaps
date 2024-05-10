483e34066e0693fd282c1fd7ad0c30a5497b02c7
PGK16POWERS <136324051+PGK16POWERS@users.noreply.github.com>
2024-05-10 10:18:59 +0200

    server stuff

--- nodeJs/node.js
+++ nodeJs/node.js
@@ -10,4 +10,8 @@ app.use(express.static("public"));
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.listen(process.env.PORT, () => {
+    console.log("Operations have begun");
 })
