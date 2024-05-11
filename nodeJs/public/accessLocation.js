2d186a1efa870bbf0752eb8783a04a80e7bbd92a
PGK16POWERS <136324051+PGK16POWERS@users.noreply.github.com>
2024-05-11 20:36:34 +0200

    js

--- nodeJs/public/accessLocation.js
+++ nodeJs/public/accessLocation.js
@@ -5,26 +5,26 @@ document.addEventListener("DOMContentLoaded", () => {
     const accessUserLocationButton = document.querySelector("#locationButton");
     const selectOption = document.querySelector("#placeOfInterest");
     const searchLocation = document.querySelector("#search_nearest_poi");
-    mapboxgl.accessToken = 'pk.eyJ1IjoicXVpY2sta3V0IiwiYSI6ImNsdWl1Zmp0YzA4cXIyaW85cWx1N2NqYWkifQ.oktkdVTSggmud8PqxSB5dg';
+    const poiIconParent = document.querySelector("#poi-icon");
     
     selectOption.addEventListener("change", () => {
         console.log(selectOption.value);
-        localStorage.setItem("place_of_interest", selectOption.value)
+        localStorage.setItem("place_of_interest", selectOption.value);
+        poiIconParent.textContent = selectOption.value;
     });
 
     accessUserLocationButton.addEventListener("click", () => {
-            accessUserLocation();
+        showMap();
     });
 
     searchLocation.addEventListener("click", () => {
         fullPassFunc();
     });
 
-    showMap();
-
 });
 
 function defaultMap() {
+    mapboxgl.accessToken = 'pk.eyJ1IjoicXVpY2sta3V0IiwiYSI6ImNsdWl1Zmp0YzA4cXIyaW85cWx1N2NqYWkifQ.oktkdVTSggmud8PqxSB5dg';
     var map = new mapboxgl.Map({
         container: 'map',
         style: 'mapbox://styles/mapbox/streets-v11',
@@ -34,64 +34,72 @@ function defaultMap() {
 }
 
 function showMap() {
+
     navigator.geolocation.getCurrentPosition((position) => {
+
         const latitude = position.coords.latitude;
         const longitude = position.coords.longitude;
+        const accessToken = 'pk.eyJ1IjoicXVpY2sta3V0IiwiYSI6ImNsdWl1Zmp0YzA4cXIyaW85cWx1N2NqYWkifQ.oktkdVTSggmud8PqxSB5dg'; 
+        const apiUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/${longitude},${latitude}.json?access_token=${accessToken}`;
+
+        // SET THE NAME OF THE USERS LOCATION
+        fetch(apiUrl)
+            .then(response => response.json())
+            .then(data => {
+                // Extract the formatted address from the response
+                const address = data.features[0].place_name;
+            
+                // Display the address on the page
+                const locationDisplay = document.querySelector("#userLocation");
+                locationDisplay.textContent = address;
+            })
+            .catch(error => {
+                console.error('Error fetching geolocation data:', error);
+            });
 
 
+        // SET THE MAP FRAME TO THE USERS LOCATION
         var map = new mapboxgl.Map({
           container: 'map',
           style: 'mapbox://styles/mapbox/streets-v11',
-          center: [longitude || 28.0456, latitude || -26.2044],
+          center: [longitude , latitude],
           zoom: 16
         });
 
-        new mapboxgl.Marker()
+        // SET PIN ON THE USERS EXACT LOCATION
+        const customMarker = document.createElement("img");
+        customMarker.setAttribute("src","/assets/marjella_icon.svg");
+        customMarker.style.width = "50px";
+        customMarker.style.height = "50px";
+
+        new mapboxgl.Marker(customMarker)
             .setLngLat([longitude, latitude])
             .addTo(map);
 
-    });
-}
+
+
+        // SET LOCATION FOR EACH BANK
+        fetch("jsonFiles/bankLocations.json")
+            .then(response =>response.json())
+            .then(data => {
     
+                data.forEach(doc => {
+                    const bankIcons = document.createElement("img");
+                    bankIcons.setAttribute("src","/assets/bank_icon.svg");
+                    bankIcons.style.width = "30px";
+                    bankIcons.style.height = "30px";
 
-function accessUserLocation() {
-    navigator.geolocation.getCurrentPosition((position) => {
-        const latitude = position.coords.latitude;
-        const longitude = position.coords.longitude;
+                    var latitudez = doc.latitude;
+                    var longitudez = doc.longitude;
 
-        // Call a function to perform reverse geocoding
-        reverseGeocode(latitude, longitude);
-
-        var coordsObj = {
-            latitude : position.coords.latitude,
-            longitude : position.coords.longitude,
-        }
-
-        var coords = JSON.stringify(coordsObj);
-
-        localStorage.setItem("user_coords", coords);
+                    new mapboxgl.Marker(bankIcons)
+                        .setLngLat([longitudez, latitudez])
+                        .addTo(map);
+                })
+            });
 
     });
-}
 
-function reverseGeocode(latitude, longitude) {
-    const accessToken = 'pk.eyJ1IjoicXVpY2sta3V0IiwiYSI6ImNsdWl1Zmp0YzA4cXIyaW85cWx1N2NqYWkifQ.oktkdVTSggmud8PqxSB5dg'; 
-    const apiUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/${longitude},${latitude}.json?access_token=${accessToken}`;
-
-    // Make a GET request to the Mapbox Geocoding API
-    fetch(apiUrl)
-        .then(response => response.json())
-        .then(data => {
-            // Extract the formatted address from the response
-            const address = data.features[0].place_name;
-
-            // Display the address on the page
-            const locationDisplay = document.querySelector("#userLocation");
-            locationDisplay.textContent = address;
-        })
-        .catch(error => {
-            console.error('Error fetching geolocation data:', error);
-        });
 }
 
 function fullPassFunc() {
@@ -100,3 +108,4 @@ function fullPassFunc() {
 
 
 }
+
