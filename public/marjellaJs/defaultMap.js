72004de5599311e8574970e4db631d572af6ad21
PGK16POWERS <136324051+PGK16POWERS@users.noreply.github.com>
2024-07-10 14:44:03 +0200

    drippsaga

--- public/marjellaJs/defaultMap.js
+++ public/marjellaJs/defaultMap.js
@@ -13,6 +13,13 @@ document.addEventListener("DOMContentLoaded", () => {
       map.on('style.load', () => {
         map.setConfigProperty('basemap', 'lightPreset', 'dawn');
         map.setConfigProperty('basemap', 'showPointOfInterestLabels', true);
+        map.setFog({
+            color: 'rgb(186, 210, 235)', // Lower atmosphere
+            'high-color': 'rgb(36, 92, 223)', // Upper atmosphere
+            'horizon-blend': 0.02, // Atmosphere thickness (default 0.2 at low zooms)
+            'space-color': 'rgb(11, 11, 25)', // Background color
+            'star-intensity': 0.6 // Background star brightness (default 0.35 at low zoooms )
+          });
       });
 
 })
