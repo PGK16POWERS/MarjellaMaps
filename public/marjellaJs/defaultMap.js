e7f3fbd1dc83e053dbbed6661faaf2f3a37d3555
PGK16POWERS <136324051+PGK16POWERS@users.noreply.github.com>
2024-07-10 12:52:03 +0200

    slimey Touches

--- public/marjellaJs/defaultMap.js
+++ public/marjellaJs/defaultMap.js
@@ -4,22 +4,15 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const map = new mapboxgl.Map({
         container: 'map', // ID of the map container element
-        style: 'mapbox://styles/mapbox/streets-v11',
+        style: 'mapbox://styles/mapbox/standard',
         center: [28.0500, -26.2000], // Default center coordinates
         zoom: 16,
-        pitch: 60
+        pitch:60
       });
 
-      map.on('load', () => {
-        map.addLayer({
-          id: 'terrain-data',
-          type: 'line',
-          source: {
-            type: 'vector',
-            url: 'mapbox://mapbox.mapbox-terrain-v2'
-          },
-          'source-layer': 'contour'
-        });
+      map.on('style.load', () => {
+        map.setConfigProperty('basemap', 'lightPreset', 'dawn');
+        map.setConfigProperty('basemap', 'showPointOfInterestLabels', true);
       });
 
 })
